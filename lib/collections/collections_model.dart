8e2d39e8dccaf1c73a249e8c21028a9b7770bde0
Yavuz Günay <yavuzgunay@gmail.com>
2024-01-19 21:10:55 +0100

    import collections

--- lib/collections/collections_model.dart
+++ lib/collections/collections_model.dart
@@ -1,53 +1,56 @@
-import 'dart:convert';
+import 'package:request_craft/request/request_model.dart';
 
-class Collection {
+class CollectionModel {
   String name;
-  String uuid;
   String description;
-  List<String> requestUuids;
+  List<RequestModel> requests;
   List<String> tags;
   String versionControlInfo;
   Map<String, dynamic> sharedVariables;
   int? customOrder;
   String? environmentBinding;
   Map<String, dynamic> customFields;
+  int version;
 
-  Collection({
+  CollectionModel({
     required this.name,
-    required this.uuid,
     required this.description,
-    required this.requestUuids,
+    required this.requests,
     required this.tags,
     required this.versionControlInfo,
     required this.sharedVariables,
     this.customOrder,
     this.environmentBinding,
     required this.customFields,
+    this.version = 1,
   });
 
-  factory Collection.fromJson(Map<String, dynamic> json) => Collection(
+  factory CollectionModel.fromJson(Map<String, dynamic> json) =>
+      CollectionModel(
         name: json['name'],
-        uuid: json['uuid'],
         description: json['description'],
-        requestUuids: List<String>.from(json['requestUuids']),
+        requests: List.from(json['requests'])
+            .map((e) => RequestModel.fromJson(e))
+            .toList(),
         tags: List<String>.from(json['tags']),
         versionControlInfo: json['versionControlInfo'],
         sharedVariables: json['sharedVariables'],
         customOrder: json['customOrder'],
         environmentBinding: json['environmentBinding'],
         customFields: json['customFields'],
+        version: json['version'],
       );
 
   Map<String, dynamic> toJson() => {
         'name': name,
-        'uuid': uuid,
         'description': description,
-        'requestUuids': requestUuids,
+        'requests': requests,
         'tags': tags,
         'versionControlInfo': versionControlInfo,
         'sharedVariables': sharedVariables,
         'customOrder': customOrder,
         'environmentBinding': environmentBinding,
         'customFields': customFields,
+        'version': version,
       };
 }
--- lib/collections/collections_screen.dart
+++ lib/collections/collections_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:request_craft/collections/collections_model.dart';
 
 class CollectionsScreen extends StatefulWidget {
   const CollectionsScreen({super.key});
@@ -8,13 +13,81 @@ class CollectionsScreen extends StatefulWidget {
 }
 
 class _CollectionsScreenState extends State<CollectionsScreen> {
+  List<CollectionModel> collections = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Text(
-        "Collections Screen",
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: () async {
+                    setState(
+                      () {
+                        collections.add(CollectionModel(
+                          name: "New collection",
+                          description: "",
+                          requests: [],
+                          tags: [],
+                          versionControlInfo: '',
+                          sharedVariables: {},
+                          customFields: {},
+                        ));
+                      },
+                    );
+                  },
+                  child: const Text("New")),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () async {
+                    await import();
+                  },
+                  child: const Text("Import")),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: collections.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(collections[index].name),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Future<void> import() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: false,
+      dialogTitle: "Pick a collection",
+      allowedExtensions: ['json'],
+      type: FileType.custom,
+      onFileLoading: (p0) {
+        print(p0);
+      },
+    );
+
+    if (result == null) {
+      print("olmaz");
+      return;
+    }
+
+    final File file = File(result.files.first.path!);
+
+    final content = file.readAsStringSync();
+    final serialized = CollectionModel.fromJson(jsonDecode(content));
+
+    setState(() {
+      collections.add(serialized);
+    });
   }
 }
